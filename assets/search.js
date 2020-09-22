c0e38d065e6a7a0fb1070c826ff3e7edd8532543
Casey <pydevcs@gmail.com>
2020-09-22 16:00:54 -0700

    Search order fixed

--- assets/search.js
+++ assets/search.js
@@ -39,10 +39,22 @@ function highlight(input, text) {
 
 function srch(item, searchText) {
   var matched = false;
+  var content = "";
   var ch_title = "";
   var section = "";
+  var title_matched = false;
+  var section_matched = false;
+  var p_matched = false;
+  var img_matched = false;
+  var is_interview = false;
+  var url = "/"
+  if (item[0].startsWith("int")){
+    url = "/interview/" + item[0].replace("int", "");
+  } else {
+    url = "/section/" + item[0];
+  }
   if (item[1].ch == "Interviews") {
-    ch_title = "Interviews:";
+    is_interview = true;
   } else {
     ch_title = highlight(item[1].ch_title, searchText);
     if (ch_title) {
@@ -53,23 +65,16 @@ function srch(item, searchText) {
     section = highlight(item[0], searchText);
     if (section) {
       matched = true;
+      section_matched = true;
     } else {
       section = item[0];
     }
   }
-  
-  var url = "/"
-  if (item[0].startsWith("int")){
-    url = "/interview/" + item[0].replace("int", "");
-  } else {
-    url = "/section/" + item[0];
-  }
   var title = highlight(item[1].title, searchText);
   if (title) {
     matched = true;
-    //title = "<h3>" + title + "</h3>";
+    title_matched = true;
   } else {
-    //title = "<h3>" + item[1].title + "</h3>";
     title = item[1].title;
   }
   //var cntnt = item[1].content;
@@ -78,7 +83,7 @@ function srch(item, searchText) {
   //console.log(content_index); 
   //<a href="https://archive.computerhistory.org/resources/access/text/2013/05/102746645-05-01-acc.pdf">Norm Abramson Interviewed by James Pelkey 10/13/88</a>
   
-  var content = "";
+  var matched_sentence = "";
   var decoded_content = decode(item[1].content);
   var matched_img = "";
   var search_rx = new RegExp(searchText, "gi");
@@ -110,17 +115,7 @@ function srch(item, searchText) {
       var shadow_img = img_dom.getElementsByTagName('IMG')[0]
       if (shadow_img) {
         if (shadow_img.src) {
-          var classname = "imgl";
-          /*
-          var img_src = shadow_img.src.split("/").pop();
-          if (img_src.includes("-c.")) {
-            classname = "imgc";
-          } else if (img_src.includes("-l.")) {
-            classname = "imgl";
-          } else if (img_src.includes("-r.")) {
-            classname = "imgr";
-          }
-          */          
+          var classname = "imgl";         
           var img_title = highlight(shadow_img.title, searchText);
           if (!img_title) {
             img_title = shadow_img.title;
@@ -134,10 +129,10 @@ function srch(item, searchText) {
               img_title + 
             "</div>";
           matched_img = formatted_img;
+          img_matched = true;
         }
       }
     }
-    var content = "";
     var search_count = 0;
     for (var i=0; i<paragraphs.length; i++) {
       if (paragraphs[i].toLowerCase().includes(text_lower)) {
@@ -146,59 +141,90 @@ function srch(item, searchText) {
           var count = (sentences[s].match(search_rx) || []).length;
           if (count > 0 && count > search_count) {
             search_count = count;
-            content = sentences[s];
-            if (content.startsWith('” ')) {
-              content = content.replace('” ', '');
+            matched_sentence = sentences[s];
+            if (matched_sentence.startsWith('” ')) {
+              matched_sentence = matched_sentence.replace('” ', '');
             }
-            if (content.startsWith(", ")) {
-              content = content.replace(", ", "");
+            if (matched_sentence.startsWith(", ")) {
+              matched_sentence = matched_sentence.replace(", ", "");
             }
           }
         }
       }
     }
   }
-  content = highlight(content, searchText);
+  content = highlight(matched_sentence, searchText);
   if (content) {
     matched = true;
+    p_matched = true;
     content = "<p>" + content + "</p>";
   } else {
     content = "";
   }
   if (matched) {
+    var search_link = "<a href='" + url + "'>";
+    var search_item = {};
     var html = 
-      `<div class="search-item">
+      `<a href="${url}" class="search-item">
         <h1>${ch_title}</h1>
         <h2>${section} ${title}</h2>
         ${content}
-        ${matched_img}
-      </div>`;
-    return html;
+        ${matched_img}</a>`;
+    //return html;
+    if (is_interview) {
+      search_item.interview = [html];      
+    }
+    else if (section_matched || title_matched) {
+      search_item.h2 = html;
+    } else if (p_matched && !section_matched && !title_matched) {
+      search_item.p = [html];
+    } else if (img_matched && !section_matched && !title_matched && !p_matched) {
+      search_item.img = [html];
+    }	
+    return search_item;
   }
 }
 
-const searchJSON = searchText => {
-  let html = "";
-  if (searchText.length < 2) { // Clear when input or matches are empty
-    hideResults();
-  } else {
-    showResults();
-    html = search_array.map( item => srch(item, searchText) ).join('');
-  }
-  document.getElementById('results').innerHTML = html;
-};
-
-let timeout = null;
+let search_timeout = null;
 document.getElementById('search').addEventListener('keyup', function (e) {
-  clearTimeout(timeout);
-  timeout = setTimeout(function () {
+  clearTimeout(search_timeout);
+  search_timeout = setTimeout(function () {
     let html = "";
     let searchText = document.getElementById('search').value;
     if (searchText.length < 2) { // Clear when input or matches are empty
       hideResults();
     } else {
       showResults();
-      html = search_array.map( item => srch(item, searchText) ).join('');
+      var search_results = {
+        "h2" : [],
+        "interviews" : [],
+        "p" : [],
+        "img" : []
+      };
+      for (var i=0; i<search_array.length; i++) {
+        let search_query = srch(search_array[i], searchText);
+        if (search_query) {
+          if (search_query.h2){
+            search_results.h2.push(search_query.h2);
+          }
+          if (search_query.interview){
+            search_results.interviews.push(search_query.interview);
+          }
+          if (search_query.p){
+            search_results.p.push(search_query.p);
+          }
+          if (search_query.img){
+            search_results.img.push(search_query.img);
+          }
+        }
+      }
+      html = search_results.h2.join("");
+      if (search_results.interviews.length > 0) {
+        html += "<div class='search-item'><h1>Interviews:</h1></div>";
+        html += search_results.interviews.join("");
+      }
+      html += search_results.p.join("");
+      html += search_results.img.join("");
     }
     document.getElementById('results').innerHTML = html;
   }, 250);
